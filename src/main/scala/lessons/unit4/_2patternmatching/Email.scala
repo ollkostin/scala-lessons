622cdbde1d768e4f8b19c3c2f35b45ca33ab2930
Vladimir Kostin <ollkostin@gmail.com>
2019-10-14 09:17:12 +0300

    Finish task from "pattern matching" topic

--- src/main/scala/lessons/unit4/_2patternmatching/Email.scala
+++ src/main/scala/lessons/unit4/_2patternmatching/Email.scala
@@ -1,30 +1,27 @@
 package lessons.unit4._2patternmatching
 
 
-/**
-  * Does not work on test #6
-  */
 object Email extends App {
   val input1 = List("oleg", "oleg@email.com", "7bdaf0a1be3", "a8af118b1a2", "28d74b7a3fe")
   val input2 = List("oleg oleg@email.com", "7bdaf0a1be3", "a8af118b1a2", "28d74b7a3fe")
+  val input3 = List("Oleg 123@gmail.com", "123@gmaiasdl.com")
+  val input4 = List("Oleg ol@gmail.com", "asdasdasdasd")
+  val inputs = List(input1, input2, input3, input4)
 
-  def getNameAndEmail(list : List[String]) : String = {
-    val nameR = "[a-zA-Z]+".r
-    val emailR = "\\w+@\\w+\\.\\w+".r
+  def getNameAndEmail(input: List[String]): String = {
+    val nameR = "([a-zA-Z]+)".r
+    val emailR = "(\\w+@\\w+\\.\\w+)".r
     val nameEmailR = "([a-zA-Z]+) (\\w+@\\w+\\.\\w+)".r
-    list match {
-      case List(nameEmailR(name, email), _*) => {
-        name + " " + email.substring(email.indexOf("@") + 1)
-      }
-      case List(name@nameR, email@emailR, _*) => {
-        name + " " + email.substring(email.indexOf("@") + 1)
-      }
+    val result = input match {
+      case List(nameEmailR(name, email), _*) =>
+        s"$name ${email.substring(email.indexOf("@") + 1)}"
+      case List(nameR(name), emailR(email), _*) =>
+        s"$name ${email.substring(email.indexOf("@") + 1)}"
       case _ => "invalid"
     }
+    result
   }
 
-  println(getNameAndEmail(input1))
-  println(getNameAndEmail(input2))
-
+  inputs.map(getNameAndEmail).foreach(println)
 
 }
